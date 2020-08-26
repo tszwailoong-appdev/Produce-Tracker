697102194e987d6cadf864e5d492a0971547651c
tszwailoong <tloong@chicagobooth.edu>
2020-08-26 01:07:38 +0000

    config comments

---
+++ app/models/comment.rb
@@ -0,0 +1,13 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id         :integer          not null, primary key
+#  body       :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  product_id :integer
+#  user_id    :integer
+#
+class Comment < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_26_010505) do
+ActiveRecord::Schema.define(version: 2020_08_26_010640) do
 
   create_table "bookmarks", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "product_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.text "body"
     t.integer "user_id"
     t.integer "product_id"
     t.datetime "created_at", precision: 6, null: false
