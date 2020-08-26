85a6a82a23a1710a615bd82a5916664f693181bc
tszwailoong <tloong@chicagobooth.edu>
2020-08-26 01:06:25 +0000

    config bookmarks

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_26_010213) do
+ActiveRecord::Schema.define(version: 2020_08_26_010505) do
+
+  create_table "bookmarks", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "product_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "farms", force: :cascade do |t|
     t.string "name"
