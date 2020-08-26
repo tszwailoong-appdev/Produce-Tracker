9d4e335f5d549bfabbdc6f9a7b475565632aac4b
tszwailoong <tloong@chicagobooth.edu>
2020-08-26 00:54:39 +0000

    Config farms db

---
+++ app/controllers/farms_controller.rb
@@ -0,0 +1,54 @@
+class FarmsController < ApplicationController
+  def index
+    matching_farms = Farm.all
+
+    @list_of_farms = matching_farms.order({ :created_at => :desc })
+
+    render({ :template => "farms/index.html.erb" })
+  end
+
+  def show
+    the_id = params.fetch("path_id")
+
+    matching_farms = Farm.where({ :id => the_id })
+
+    @the_farm = matching_farms.at(0)
+
+    render({ :template => "farms/show.html.erb" })
+  end
+
+  def create
+    the_farm = Farm.new
+    the_farm.name = params.fetch("query_name")
+
+    if the_farm.valid?
+      the_farm.save
+      redirect_to("/farms", { :notice => "Farm created successfully." })
+    else
+      redirect_to("/farms", { :notice => "Farm failed to create successfully." })
+    end
+  end
+
+  def update
+    the_id = params.fetch("path_id")
+    the_farm = Farm.where({ :id => the_id }).at(0)
+
+    the_farm.name = params.fetch("query_name")
+
+    if the_farm.valid?
+      the_farm.save
+      redirect_to("/farms/#{the_farm.id}", { :notice => "Farm updated successfully."} )
+    else
+      redirect_to("/farms/#{the_farm.id}", { :alert => "Farm failed to update successfully." })
+    end
+  end
+
+  def destroy
+    the_id = params.fetch("path_id")
+    the_farm = Farm.where({ :id => the_id }).at(0)
+
+    the_farm.destroy
+
+    redirect_to("/farms", { :notice => "Farm deleted successfully."} )
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,23 @@ Rails.application.routes.draw do
 
 
 
+  # Routes for the Farm resource:
+
+  # CREATE
+  post("/insert_farm", { :controller => "farms", :action => "create" })
+          
+  # READ
+  get("/farms", { :controller => "farms", :action => "index" })
+  
+  get("/farms/:path_id", { :controller => "farms", :action => "show" })
+  
+  # UPDATE
+  
+  post("/modify_farm/:path_id", { :controller => "farms", :action => "update" })
+  
+  # DELETE
+  get("/delete_farm/:path_id", { :controller => "farms", :action => "destroy" })
+
+  #------------------------------
+
 end
